11711b841f7bb040bc4fea2a5c569549e46a796f
dbaileyc <103800109+dbaileyc@users.noreply.github.com>
2022-08-15 13:45:38 -0400

    frontendhome

---
+++ src/app/item-details/item-details.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Item } from '../item';
+import { ActivatedRoute } from '@angular/router';
+import { ItemService } from '../item.service';
+
+@Component({
+  selector: 'app-item-details',
+  templateUrl: './item-details.component.html',
+  styleUrls: ['./item-details.component.css']
+})
+export class ItemDetailsComponent implements OnInit {
+
+  id!: number
+  item!: Item
+  constructor(private route: ActivatedRoute, private itemService: ItemService) { }
+
+  ngOnInit(): void {
+    this.id = this.route.snapshot.params['id'];
+
+    this.item= new Item();
+    this.itemService.getItemById(this.id).subscribe( data => {
+      this.item = data;
+    });
+  }
+
+}
