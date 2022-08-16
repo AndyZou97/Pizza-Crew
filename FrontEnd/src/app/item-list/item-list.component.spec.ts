12dd7e4b60ac43cb9b4c027f7342fc04605a50b8
Andy Zou <azou@xpanxion.com>
2022-08-16 12:31:11 -0400

    Modified the item list HTML

--- FrontEnd/src/app/item-list/item-list.component.spec.ts
+++ FrontEnd/src/app/item-list/item-list.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { itemListComponent } from './item-list.component';
+import { ItemListComponent } from './item-list.component';
 
 describe('itemListComponent', () => {
-  let component: itemListComponent;
-  let fixture: ComponentFixture<itemListComponent>;
+  let component: ItemListComponent;
+  let fixture: ComponentFixture<ItemListComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ itemListComponent ]
+      declarations: [ ItemListComponent ]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(itemListComponent);
+    fixture = TestBed.createComponent(ItemListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
