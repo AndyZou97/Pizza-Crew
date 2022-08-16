0722bae725fe1b14871f5d4be0cb47a15e9d177e
AzureAD\KyranBaker <kbaker@xpanxion.com>
2022-08-16 11:40:29 -0400

    MenuUpdate

--- FrontEnd/src/app/menu/menu.component.ts
+++ FrontEnd/src/app/menu/menu.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import {Item} from '../item'
+import { ItemService } from '../item.service';
+
 
 @Component({
   selector: 'app-menu',
@@ -7,9 +11,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
-  constructor() { }
+  items!:Item[];
 
-  ngOnInit(): void {
-  }
+  constructor(private itemService:ItemService,
+    private router:Router) { }
+
+    ngOnInit(): void {
+      this.getItems();
+    }
+  
+    private getItems(){
+      this.itemService.getItemList().subscribe(data => {
+        this.items = data
+      })
+    }
+  
+    updateItem(id:number){
+      this.router.navigate(['update-item',id])
+  
+    }
+    deleteItem(id:number){
+      this.itemService.deleteItem(id).subscribe(data =>{
+        this.getItems();
+      })
+  
+    }
+  
+    itemDetails(id:number){
+      this.router.navigate(['item-details',id])
+  
+    }
+
+ 
 
 }
